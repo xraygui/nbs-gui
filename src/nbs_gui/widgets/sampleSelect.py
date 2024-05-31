1d73fcc104e822d85a53d160633f0296d3819a6a
Charles Titus <ctitus@bnl.gov>
2024-05-30 22:08:08 -0400

    Added manual sample addition button

--- src/nbs_gui/widgets/sampleSelect.py
+++ src/nbs_gui/widgets/sampleSelect.py
@@ -6,12 +6,16 @@ from qtpy.QtWidgets import (
     QPushButton,
     QLineEdit,
     QLabel,
+    QDialog,
+    QVBoxLayout,
+    QLineEdit,
+    QDialogButtonBox,
 )
-from qtpy.QtCore import Signal, QObject
+from qtpy.QtCore import Signal, QObject, Slot, Qt
 from .status import StatusBox
 from .manipulator_monitor import RealManipulatorMonitor
 from qtpy.QtGui import QDoubleValidator
-from bluesky_queueserver_api import BPlan
+from bluesky_queueserver_api import BPlan, BFunc
 
 
 class SampleSelectModel(QObject):
@@ -35,7 +39,45 @@ class SampleSelectModel(QObject):
         return plan
 
 
+class SampleDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Add Current Position as Sample")
+        layout = QVBoxLayout(self)
+
+        # Sample ID
+        hbox_id = QHBoxLayout()
+        hbox_id.addWidget(QLabel("Sample ID:"))
+        self.sample_id_edit = QLineEdit(self)
+        hbox_id.addWidget(self.sample_id_edit)
+        layout.addLayout(hbox_id)
+
+        # Sample Name
+        hbox_name = QHBoxLayout()
+        hbox_name.addWidget(QLabel("Sample Name:"))
+        self.sample_name_edit = QLineEdit(self)
+        hbox_name.addWidget(self.sample_name_edit)
+        layout.addLayout(hbox_name)
+
+        # Description
+        hbox_desc = QHBoxLayout()
+        hbox_desc.addWidget(QLabel("Description:"))
+        self.description_edit = QLineEdit(self)
+        hbox_desc.addWidget(self.description_edit)
+        layout.addLayout(hbox_desc)
+
+        # Dialog buttons
+        self.buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self
+        )
+        layout.addWidget(self.buttons)
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+
 class SampleSelectWidget(QGroupBox):
+    signal_update_widget = Signal(bool, object)
+
     def __init__(self, model, parent=None, **kwargs):
         super().__init__("Sample Selection", parent=parent)
         self.run_engine = model.run_engine
@@ -55,6 +97,9 @@ class SampleSelectWidget(QGroupBox):
         self.y.setValidator(QDoubleValidator())
         self.r = QLineEdit("45")
         self.r.setValidator(QDoubleValidator())
+        self.add_button = QPushButton("Add Current Position as New Sample")
+        vbox.addWidget(self.add_button)
+        self.add_button.clicked.connect(self.add_current_position)
 
         hbox = QHBoxLayout()
         hbox.addWidget(QLabel("x"))
@@ -69,6 +114,18 @@ class SampleSelectWidget(QGroupBox):
         vbox.addWidget(self.cb2)
         vbox.addWidget(self.button)
         self.setLayout(vbox)
+        self.run_engine.events.status_changed.connect(self.slot_update_widgets)
+
+    @Slot(object)
+    def slot_update_widgets(self, event):
+        is_connected = bool(event.is_connected)
+        status = event.status
+        # 'is_connected' takes values True, False
+        worker_exists = status.get("worker_environment_exists", False)
+        running_item_uid = status.get("running_item_uid", None)
+
+        enabled = is_connected and worker_exists and not bool(running_item_uid)
+        self.button.setEnabled(enabled)
 
     def update_samples(self, samples):
         self.cb.clear()
@@ -84,3 +141,14 @@ class SampleSelectWidget(QGroupBox):
         origin = self.cb2.currentData()
         plan = BPlan("sample_move", x, y, r, sample, origin=origin)
         self.run_engine._client.item_execute(plan)
+
+    def add_current_position(self):
+        dialog = SampleDialog(self)
+        if dialog.exec_():
+            sample_id = dialog.sample_id_edit.text()
+            sample_name = dialog.sample_name_edit.text()
+            description = dialog.description_edit.text()
+            plan = BFunc(
+                "add_current_position_as_sample", sample_name, sample_id, description
+            )
+            self.run_engine._client.function_execute(plan)
